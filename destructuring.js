3cbf05b8d813d39efda3d59b9443d0ce68b49a8b
jswelsh <james.sinclair.welsh@gmail.com>
2020-03-28 13:40:17 -0700

    destructering exercise

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+/* 
+Destructuring assignment is special syntax introduced in ES6, 
+for neatly assigning values taken directly from an object.
+
+Consider the following ES5 code:
+
+const user = { name: 'John Doe', age: 34 };
+
+const name = user.name; // name = 'John Doe'
+const age = user.age; // age = 34
+Here's an equivalent assignment statement using the ES6 destructuring syntax:
+
+*/

d8adbd6d8719108f339947c004af326055657ed7
jswelsh <james.sinclair.welsh@gmail.com>
2020-03-28 15:18:13 -0700

    finished template literals exer

--- template_literals.js
+++ template_literals.js
@@ -42,3 +42,25 @@ Use an iterator method (any kind of loop) to get the desired output (shown below
   '<li class="text-warning">linebreak</li>'
 ]
 */
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    "use strict";
+  
+    // Only change code below this line
+    const resultDisplayArray = []
+    arr.forEach(element => {
+        resultDisplayArray.push(`<li class="text-warning">${element}</li>`)
+    });
+    // Only change code above this line
+  
+    return resultDisplayArray;
+  }
+  
+  const resultDisplayArray = makeList(result.failure);
+  
+  console.log(resultDisplayArray);

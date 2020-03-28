f55cda54bc65d194fe508237ac1b9f5c716a56ec
jswelsh <james.sinclair.welsh@gmail.com>
2020-03-28 13:24:53 -0700

    rest parameters exercise

---
+++ rest_parameter.js
@@ -0,0 +1,10 @@
+/* 
+The rest parameter eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array.
+
+Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+*/
+const sum = (...args) => {
+    //const args = [x, y, z];
+    return args.reduce((a, b) => a + b, 0);
+  }
+  console.log(sum(1,2,3,4))

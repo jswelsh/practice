dbbeaea5056abebfb6bbeb769fa79a486a1ccdbe
jswelsh <james.sinclair.welsh@gmail.com>
2020-03-28 13:30:25 -0700

    spread arguments into function

--- spread.js
+++ spread.js
@@ -9,4 +9,16 @@ var maximus = Math.max.apply(null, arr); // returns 89
 We had to use Math.max.apply(null, arr) because Math.max(arr) returns NaN. 
 Math.max() expects comma-separated arguments, but not an array. The 
 spread operator makes this syntax much better to read and maintain.
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr); // returns 89
+...arr returns an unpacked array. In other words, it spreads the array. However, the spread operator only works in-place, like in an argument to a function or in an array literal. The following code will not work:
+
+const spreaded = ...arr; // will throw a syntax error
+Copy all contents of arr1 into another array arr2 using the spread operator.
 */
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [];  // Change this line
+
+console.log(arr2);
